1d99c0e548dd7e81cf23b4c7eb10b6bef6c8f495
ManInTheMirrow <75084095+ManInTheMirrow@users.noreply.github.com>
2023-07-02 00:34:57 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# android_device_blackview_Tab12_NEU
-TWRP Device Tree for Blackview Tab 12.
+# TWRP device tree for Blackview Tab 12
+
+## Device specification
+
+Basic   | Spec Sheet
+-------:|:------------------------
+CPU     | Octa-core 1.6 GHz
+CHIPSET | Unisoc SC9863A
+GPU     | PowerVR GE8322
+Memory  | 4GB
+Shipped Android Version | 11.0
+Storage | 64GB
+Battery | Li-Po 6580 mAh
+Dimensions | 238.5 x 157.5 x 7.4 mm
+Display | 1200 x 1920 pixels, 16:10 ratio (224 ppi density)
+Rear Camera  | 13 MP
+Front Camera | 5 MP
+
+![Device Picture](https://fdn2.gsmarena.com/vv/pics/blackview/blackview-Tab12.jpg)
---
+++ twrp_Tab12_NEU.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from Tab12_NEU device
+$(call inherit-product, device/blackview/Tab12_NEU/device.mk)
+
+# Inherit some common TWRP stuff
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Tab12_NEU
+PRODUCT_NAME := twrp_Tab12_NEU
+PRODUCT_BRAND := Blackview
+PRODUCT_MODEL := Tab_12
+PRODUCT_MANUFACTURER := Blackview
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=Tab12_NEU \
+    BUILD_PRODUCT=Tab12_NEU \
+	PRODUCT_NAME=Tab12_NEU \
+    PRIVATE_BUILD_DESC="Tab12_RUU-user 11 RP1A.201005.001 20220705 release-keys"
+
+BUILD_FINGERPRINT := Blackview/Tab12_RUU/Tab12_NEU:11/RP1A.201005.001/20220705:user/release-keys
